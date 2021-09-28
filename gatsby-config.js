664cd1a515531f9f342a96c44f0667041408dba4
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2021-09-28 06:05:41 +0000

    new update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Rafique Adam Cudjoe',
     description:
       'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://rafiqueadam.me', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@rafiqueacudjoe',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
