736d338cb9aee59fb79c11d7a1f505149e5b14d9
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2021-11-06 08:29:42 +0000

    new update

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Rafique Adam Cudjoe',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Rafique Adam Cudjoe is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://rafiqueadam.me', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@rafiqueacudjoe',
