e785b7626df8497d28cbe8460884498bf8d7caf0
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2024-10-18 08:25:47 +0000

    updates

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,7 +59,7 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Rafique Adam Cudjoe.</h2>;
+  const two = <h2 className="big-heading">Rafique.</h2>;
   const three = <h3 className="big-heading">I build amazing things for the web.</h3>;
   const four = (
     <>
